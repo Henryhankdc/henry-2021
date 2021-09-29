020d1caeb7b053175674f63fe95a031b03d538fe
Henry Mesias <henrymesias@gmail.com>
2021-09-28 20:42:15 -0400

    style: custom header and list styles

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,36 @@ export default function Home() {
 
           </div>
           <div className="content">
-            <h1 className="content-heading mt-3">Henry Mesias</h1>
+            <h1 className="content-headingOne mt-3 c-blueLight">Henry Mesias</h1>
             <p className="content-story">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur consectetur placeat modi mollitia blanditiis, rerum ea earum sunt consequatur? Quibusdam natus delectus accusantium, perspiciatis consectetur doloremque odio! Laboriosam, minima eat? </p>
             <p className="content-story mt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur consectetur placeat modi mollitia blanditiis, rerum ea earum sunt consequatur? Quibusdam natus delectus accusantium, perspiciatis consectetur doloremque odio! Laboriosam, minima eat? </p>
+
+            <div className="content-projects mt-3 mb-3">
+              <h2 className="content-headingTwo c-blueLight">projects</h2>
+              <ul className="content-list mt-2">
+                <li className="content-item mb-2">
+                  <div className="content-item-wrapper">
+                    <p className="content-item-title"><a href="#FIXME" className="content-item-link">Example Project Link</a></p>
+                    <p className="content-item-description c-gray">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui fuga fugit laborum cum debitis. Expedita ipsum in et praesentium, quas at exercitationem quod cupiditate aperiam ipsa maiores eum, pariatur nostrum? </p>
+                  </div>
+                </li>
+                <li className="content-item mb-2">
+                  <div className="content-item-wrapper">
+                    <p className="content-item-title"><a href="#FIXME" className="content-item-link">Example Project Link</a></p>
+                    <p className="content-item-description c-gray">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui fuga fugit laborum cum debitis. Expedita ipsum in et praesentium, quas at exercitationem quod cupiditate aperiam ipsa maiores eum, pariatur nostrum? </p>
+                  </div>
+                </li>
+              </ul>
+              
+            </div>
+
+            <div className="content-experience mt-3 mb-3">
+              <h2 className="content-headingTwo c-blueLight">Experience</h2>
+            </div>
+
+            <div className="content-tools mt-3 mb-3">
+              <h2 className="content-headingTwo c-blueLight">Tools</h2>
+            </div>
 
             <footer className={styles.footer}>
               <a
